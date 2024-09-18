7a301496d3e96277e77777c748d998f0a90a588e
Ishmael Kwayisi <spyrorocks324@gmail.com>
2024-09-17 20:50:27 -0400

    PlayerMovement Progress!
    The player can now move in all directions! You can do this with the WASD Keys or a game controller!

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,15 +9,22 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float movSpeed;
+    [SerializeField] private float movSpeed = 5f;
     private Vector2 movement;
+    private Animator animator;
+
+    private const string horizontal = "Horizontal";
+    private const string vertical = "Vertical";
+    private const string lastHorizontal = "LastHorizontal";
+    private const string lastVertical = "LastVertical";
+
     Rigidbody2D rb;
 
     // Start is called before the first frame update
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-
+        animator = rb.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -26,5 +33,14 @@ public class PlayerMovement : MonoBehaviour
         movement.Set(InputManager.movement.x, InputManager.movement.y);
 
         rb.velocity = movement * movSpeed;
+        
+        animator.SetFloat(horizontal, rb.velocity.x);
+        animator.SetFloat (vertical, rb.velocity.y);
+
+        if(movement != Vector2.zero)
+        {
+            animator.SetFloat(lastHorizontal, movement.x);
+            animator.SetFloat(lastVertical, movement.y);
+        }
     }
 }
