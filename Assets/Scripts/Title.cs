bae1155c9006339dd41b30c2881cd908e525dae2
Ishmael Kwayisi <spyrorocks324@gmail.com>
2024-11-15 13:02:45 -0500

    100% Game Over Scene

--- Assets/Scripts/Title.cs
+++ Assets/Scripts/Title.cs
@@ -11,7 +11,7 @@ public class Title : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space)) {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
