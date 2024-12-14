41d2f128f750e61352703e3842d65b05f6482b0d
Ishmael Kwayisi <spyrorocks324@gmail.com>
2024-12-14 18:52:21 -0500

    Fixing Red Throne Room Triggers
    The player should be able to now trigger the boss fight by pressing the "I" key on the keyboard in the Red Throne Room.

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -61,12 +61,4 @@ public class SceneTransition : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            SceneManager.LoadScene("StartRoomScene");
-        }
-    }
 }
